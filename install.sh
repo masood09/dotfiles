94878241319f4c411b08ddf529ecaddc90f7863b
Masood Ahmed <git@ahmedmasood.com>
2025-06-25 20:27:59 -0400

    Starting install script

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+
+# (thanks https://github.com/atomantic/dotfiles/blob/main/install.sh)
+source ./lib_sh/echos.sh
+source ./lib_sh/requirers.sh
+
+bot "Hi! I'm going to install tooling and tweak your system settings. Here I go..."
+
